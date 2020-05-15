93085cdee23fabd02059cbb209d274a2ba7de3e2
noah172 <jsebastian.garcia1995@gmail.com>
2020-05-15 17:36:19 -0500

    added: reaction to the check and unchek checkbox

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -2,4 +2,14 @@
 
 document.addEventListener('DOMContentLoaded', function (data, status) {
   let cache_id = document.getElementById('cache_id').value;
+  let dic = {};
+  $('.amenities > h4')
+  $('input[type="checkbox"]').click(function(){
+    if($(this).is(":checked")){
+      dic[$(this).data('id')] = $(this).data('name');
+    }
+    else if($(this).is(":not(:checked)")){
+      delete dic[$(this).data('id')];
+    }
+  });
 });
