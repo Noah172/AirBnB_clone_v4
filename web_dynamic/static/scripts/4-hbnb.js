34f9f0ec48e38e7308d77cc6061360443512aea9
noah172 <jsebastian.garcia1995@gmail.com>
2020-05-18 19:49:23 -0500

    filtering

--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -16,31 +16,74 @@ document.addEventListener('DOMContentLoaded', function (data, status) {
   });
 
   // verify status of the api
-  $.get('http://0.0.0.0:5001/api/v1/status/', function (data) {
-    if (data.status === 'OK') {
-      $('#api_status').addClass('available');
-    } else {
-      $('#api_status').removeClass('available');
-    }
-  });
-
-  // make the post
-  $('button').click(function(){
-    let jdic = JSON.jsonify(dic);
-    $.post('http://0.0.0.0:5001/api/v1/places_search/', jdic, function(data, status) {
-    alert('post sent: ' + data + ', status: ' + status)
-    });
-  
-
-    // takes the dic from the api
-
-    $.get('http://0.0.0.0:5001/api/v1/places_search/', function (data) {
-      const places = data;
-      let place;
-      for (let index = 0; index < places.lenght; index++) {
-        place = '<li>'+places[index]['name']+'</li>';
-        $('article').append(place);
+	$.get('http://127.0.0.1:5001/api/v1/status/', (data, code) => {
+		if (code == 'success') {
+      if (data.status != 'OK') {
+        $('#api_status').removeClass('available')
+      } else {
+        $('#api_status').addClass('available')
       }
-    });
+		}
   });
+
+  // takes the dic from the api
+  $.ajax({
+		type: 'POST',
+		url: 'http://127.0.0.1:5001/api/v1/places_search',
+		data: '{}',
+		dataType: 'json',
+		contentType: 'application/json',
+		success: function (data) {
+			for (let i = 0; i < data.length; i++) {
+				let place = data[i];
+				let placeHolder = `
+        <article>
+          <div class="title_box">
+				    <h2>${place.name}</h2>
+            <div class="price_by_night"><p>$ ${place.price_by_night}</p></div>
+          </div>
+				  <div class="information">
+				  <div class="max_guest"><p>${place.max_guest}</p></div>
+				  <div class="number_rooms"><p>${place.number_rooms}</p></div>
+				  <div class="number_bathrooms"><p>${place.number_bathrooms}</p></div>
+				  </div>
+				  <div class="description"><p>${place.description}</p></div>
+				</article>
+				`
+				$('.places ').append(placeHolder);
+			}
+		}
+	});
+  //clicking the button
+  $('.filters > button').click( () => {
+    $('.places > article').remove();
+    
+		$.ajax({
+			type: 'POST',
+			url: 'http://127.0.0.1:5001/api/v1/places_search',
+			data: JSON.stringify({'amenities': Object.keys(dic)}),
+			dataType: 'json',
+			contentType: 'application/json',
+			success: function (data) {
+				for (let i = 0; i < data.length; i++) {
+					let place = data[i];
+					let placeHolder = `
+          <article>
+            <div class="title_box">
+				      <h2>${place.name}</h2>
+              <div class="price_by_night"><p>$ ${place.price_by_night}</p></div>
+            </div>
+		  		  <div class="information">
+	  			    <div class="max_guest"><p>${place.max_guest}</p></div>
+  				    <div class="number_rooms"><p>${place.number_rooms}</p></div>
+				      <div class="number_bathrooms"><p>${place.number_bathrooms}</p></div>
+				    </div>
+				    <div class="description"><p>${place.description}</p></div>
+			  	</article>
+					`
+					$('.places ').append(placeHolder);
+				}
+			}
+		});
+	});
 });
